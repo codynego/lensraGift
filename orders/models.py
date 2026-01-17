14faa2da2812f8f387e4ec275d51ab042a71a42f
abednego emonena <emonenaabednego@gmail.com>
2026-01-17 12:11:44 +0100

    feat: Import uuid module for future use in CartItem and OrderItem models

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from products.models import Product, DesignPlacement, ProductVariant
+import uuid
 
 class CartItem(models.Model):
     user = models.ForeignKey(
