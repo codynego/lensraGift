1912c98cd305dae7d4f761c4c7c174384ab929d7
abednego emonena <emonenaabednego@gmail.com>
2026-01-24 12:13:36 +0100

    feat: Enhance image URL handling with Cloudinary transformations for optimization

--- lensra/settings.py
+++ lensra/settings.py
@@ -252,6 +252,8 @@ CLOUDINARY_STORAGE = {
     "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
     "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
     "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+    "SECURE": True,
+    "STATIC_IMAGES_EXTENSIONS": ['jpg', 'jpeg', 'png', 'gif', 'svg', 'webp'],
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
