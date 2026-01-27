93b4a4acfd45379d78ee3282e1d88dc84eee130e
abednego emonena <emonenaabednego@gmail.com>
2026-01-27 19:30:52 +0100

    feat: Update BlogPost model to use CloudinaryField for featured images and enhance BlogPostSerializer with featured_image_url method

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django_ckeditor_5.fields import CKEditor5Field
+from cloudinary.models import CloudinaryField
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    featured_image = models.ImageField(upload_to='blog_images/')
+    featured_image = CloudinaryField(
+        "blog_featured_image",
+        blank=True,
+        null=True
+    )
     # This field provides the Bold, Fonts, and Styling tools
     content = CKEditor5Field('Content', config_name='extends') 
     created_at = models.DateTimeField(auto_now_add=True)
--- blog/serializers.py
+++ blog/serializers.py
@@ -2,9 +2,30 @@ from rest_framework import serializers
 from .models import BlogPost
 
 class BlogPostSerializer(serializers.ModelSerializer):
+    featured_image_url = serializers.SerializerMethodField()
     class Meta:
         model = BlogPost
         fields = [
             'id', 'title', 'slug', 'featured_image', 
-            'content', 'created_at', 'is_published'
+            'content', 'created_at', 'is_published', 'featured_image_url'
         ]
+
+    def get_featured_image_url(self, obj):
+        if obj.featured_image:
+            url = obj.featured_image.url
+            
+            # Add Cloudinary transformations for optimization
+            # This works if you're using CloudinaryField
+            if hasattr(obj.featured_image, 'build_url'):
+                url = obj.featured_image.build_url(
+                    quality='auto',           # Automatic quality adjustment
+                    fetch_format='auto',      # Automatic format (WebP for supported browsers)
+                    width=800,                # Resize to appropriate width
+                    crop='limit',             # Don't upscale, only downscale
+                    flags='progressive'       # Progressive JPEG loading
+                )
+            
+            if url.startswith('http://'):
+                url = 'https://' + url[7:]
+            return url
+        return None
