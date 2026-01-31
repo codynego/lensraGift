98fa979c4ead2c80d5e5b9c26d86e9bd84a1f15a
abednego emonena <emonenaabednego@gmail.com>
2026-01-31 18:51:27 +0100

    fix: Import EmailSubscribeView in urls.py for subscription endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import UserRegistrationView, UserLoginView, UserProfileView, UserLogoutView
-from .views import AddressListCreateView, AddressDetailView, SetDefaultAddressView
+from .views import AddressListCreateView, AddressDetailView, SetDefaultAddressView, EmailSubscribeView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
