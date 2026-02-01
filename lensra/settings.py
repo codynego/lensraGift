4839560a60107ea06245f09aa32ed247b32f19ad
abednego emonena <emonenaabednego@gmail.com>
2026-02-01 15:07:18 +0100

    feat: Add Redis configuration for caching and Celery broker

--- lensra/settings.py
+++ lensra/settings.py
@@ -283,3 +283,23 @@ CELERY_TASK_SERIALIZER = "json"
 INSTALLED_APPS += ["django_celery_results"]
 
 CELERY_RESULT_BACKEND = "django-db"
+
+REDIS_PASSWORD = config('REDIS_PASSWORD', default='STRONG_PASSWORD_HERE')
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://:{REDIS_PASSWORD}@127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
+CELERY_BROKER_URL = f"redis://:{REDIS_PASSWORD}@127.0.0.1:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_BACKEND = "django-db"
+
